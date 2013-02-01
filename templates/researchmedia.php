c754e75b71c3f10bfe0f55e09a6dc9fe613829d8
Josua Leonard <josua.leonard@gmail.com>
2013-02-01 17:32:07 +0700

    Fix toolbox issue #10, add icon

--- templates/researchmedia.php
+++ templates/researchmedia.php
@@ -6,7 +6,15 @@
 
 <li media-id="<?=$attachment_id?>" media-type="<?=$template_params['media_type']?>" media-caption="<?=$media_caption?>" media-description="<?=$template_params['media_description']?>">
   <a href="#" class="thumbnail research-media">
-    <img src="<?=$template_params['attachment_thumb']?>" id="thumb_<?=$attachment_id?>" />
+    <img src="<?=$template_params['attachment_thumb']?>" id="thumb_<?=$attachment_id?>" class="fade in" />
+    <?php
+      if($template_params['media_type']=='photo'){
+        echo '<div class="icon-camera-2x fade"></div>';
+      }
+      else {
+        echo '<div class="icon-play-2x fade"></div>';
+      }
+    ?>
   </a>
   <div class="pull-left">
     <?php
