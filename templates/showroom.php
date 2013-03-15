4ecc082aed6c28e183ac86f739cc8f5730de0e1e
Josua Leonard <josua.leonard@gmail.com>
2013-03-15 12:10:56 +0700

    Fix thumb trash touch on mobile

--- templates/showroom.php
+++ templates/showroom.php
@@ -10,16 +10,17 @@
 <li media-id="<?=$attachment_id?>" media-type="<?=$media_type?>" media-caption="<?=$template_params['media_caption']?>" media-description="<?=$template_params['media_description']?>"<?=($taxonomy && $taxonomy!="")?'taxonomy="'.$taxonomy.'"':""?><?=$attr_favorite?>>
   <a class="thumbnail">
     <img src="<?=$template_params['attachment_thumb']?>" id="thumb_<?=$attachment_id?>" />
-    <div class="thumb-trash"><i class="icon-trash"></i></div>
   </a>
-  <div class="pull-right">
-    <?php
-    if ($template_params['favorite']) {
-      echo '<div class="thumb-favorite thumb-favorite-selected" data-target="'.$attachment_id.'" rel="tooltip" data-placement="top" title="Your favorite photo of this project."><i class="icon-ok icon-white"></i></div>';
-    } else {
-      echo '<div class="thumb-favorite" data-target="'.$attachment_id.'" rel="tooltip" data-placement="top" title="Mark the photo as your favorite."><i class="icon-ok icon-white"></i></div>';
-    }
-    ?>
-    <!--a href="javascript:void(0);" class="action-tag-media preventDefault" >Describe this <?=$media_type?> </a><icon class="icon-tags"></icon-->
-  </div>
+  <div class="mobile-spacer"></div>
+  <div class="thumb-trash"><i class="icon-trash"></i></div>
+  <?php
+  if ($template_params['favorite']) {
+    echo '<div class="thumb-favorite thumb-favorite-selected" data-target="'.$attachment_id.'" rel="tooltip" data-placement="top" title="Your favorite photo of this project."><i class="icon-ok icon-white"></i></div>';
+  } else {
+    echo '<div class="thumb-favorite" data-target="'.$attachment_id.'" rel="tooltip" data-placement="top" title="Mark the photo as your favorite."><i class="icon-ok icon-white"></i></div>';
+  }
+  ?>
+  <!--div class="pull-right">
+    <a href="javascript:void(0);" class="action-tag-media preventDefault" >Describe this <?=$media_type?> </a><icon class="icon-tags"></icon>
+  </div-->
 </li>
