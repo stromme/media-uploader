6f32a288dda8b4e566a685a13e03c2cffc93d978
Josua Leonard <josua.leonard@gmail.com>
2013-05-16 13:33:42 +0700

    Fix plupload

--- mediauploader.js
+++ mediauploader.js
@@ -26,7 +26,7 @@ $.fn.mediauploader = function() {
         if(elmSettings.data_target_id && elmSettings.data_target_id!=''){
           // Attach hidden uploader box
           var uploader_id = "uploader_"+elmSettings.data_target_id;
-          var plupload_basic = "<div id='"+uploader_id+"' style='"+/*display:none;visibility:hidden;*/"' class='plupload-uploader-container'></div>";
+          var plupload_basic = "<div id='"+uploader_id+"' style='display:none;visibility:hidden;' class='plupload-uploader-container'></div>";
           $(plupload_basic).insertAfter(elm);
           elmBaseSettings["browse_button"] = elm.attr('id');
           elmBaseSettings["container"] = uploader_id;
@@ -40,9 +40,8 @@ $.fn.mediauploader = function() {
           var uploader = new plupload.Uploader(elmBaseSettings);
           //uploader.bind('Init', function(up){});
           uploader.init();
-          console.log(elmBaseSettings);
-          $('#'+uploader_id+' div').removeAttr('style');
-          $('#'+uploader_id+' input').removeAttr('style');
+          //$('#'+uploader_id+' div').removeAttr('style');
+          //$('#'+uploader_id+' input').removeAttr('style');
           // When a file was added in the queue
           uploader.bind('FilesAdded', function(up, files){
             $.each(files, function(i, file) {
